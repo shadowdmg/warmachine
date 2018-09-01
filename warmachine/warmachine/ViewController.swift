60620fbcc0c799342b5324c9b428933b3073edd6
pchan <shadowdmg@gmail.com>
2018-08-31 23:51:51 -0700

    more dev

--- warmachine/warmachine/ViewController.swift
+++ warmachine/warmachine/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        let rand1 = arc4random_uniform(13) + 2
+        let rand2 = arc4random_uniform(13) + 2
+        
+        leftImageView.image = UIImage(named: "card\(rand1)")
+        rightImageView.image = UIImage(named: "card\(rand2)")
 
+        if (rand1 > rand2)
+        {
+            leftScore+=1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if (rand1 < rand2)
+        {
+            rightScore+=1
+            rightScoreLabel.text = String(rightScore)
+        }
+    }
+    
 }
 
